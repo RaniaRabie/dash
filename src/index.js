83b056a2862bbb98816e28063ce1070b0edb3628
RaniaRabie <167593676+RaniaRabie@users.noreply.github.com>
2024-10-28 22:54:04 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,81 @@
+/*
+- File Name: index.js
+- Author: shrouk ahmed
+- Date of Creation: 17/9/2024
+- Versions Information: 1.0.0
+- Dependencies:
+  {
+  REACT , 
+  MUI ,
+  react-router-dom ,
+  App.css,
+  index.css,
+  Dashboard ,
+  BarChart ,
+  PieChart ,
+  LineChart ,
+  AllUsers ,
+  AddNewUser ,
+  Profile   ,
+  CreateRoadmap , 
+  RoadmapDetails,
+  RoadmapContext 
+  }
+- Contributors: shrouk ahmed
+- Last Modified Date: 17/10/2024
+- Description : main file
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import App from "./App";
+import Dashboard from "./pages/dashboard/Dashboard";
+import BarChart from "./pages/Analystics/BarChart";
+import PieChart from "./pages/Analystics/PieChart";
+import LineChart from "./pages/Analystics/LineChart";
+import AllUsers from "./pages/form/users/AllUsers";
+import AddNewUser from "./pages/form/users/AddNewUser";
+import Profile from "./pages/form/users/Profile";
+import Faq from "./pages/faq/FAQ";
+import Setting from "./pages/setting/Setting";
+import AllRoadmaps from "./pages/roadmap/create/AllRoadmaps";
+import CreateRoadmap from "./pages/roadmap/create/CreateRoadmap";
+import RoadmapDetails from "./pages/roadmap/create/Roadmapdetails";
+import { RoadmapProvider } from "./pages/roadmap/create/RoadmapContext";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Dashboard newUsersCount={undefined} />} />
+      <Route path="barchart" element={<BarChart />} />
+      <Route path="piechart" element={<PieChart />} />
+      <Route path="linechart" element={<LineChart />} />
+      <Route path="allusers" element={<AllUsers />} />
+      <Route path="addnewuser" element={<AddNewUser />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="faq" element={<Faq />} />
+      <Route path="setting" element={<Setting />} />
+      <Route path="allroadmaps" element={<AllRoadmaps />} />
+      <Route path="create" element={<CreateRoadmap />} />
+      <Route path="details" element={<RoadmapDetails />} />
+      <Route path="/create/:id" element={<CreateRoadmap />} />
+      <Route path="details/:id" element={<RoadmapDetails />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RoadmapProvider>
+      <RouterProvider router={router} />
+    </RoadmapProvider>
+  </React.StrictMode>
+);
